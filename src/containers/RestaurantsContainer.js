3c910aa3942ccb90dc7570ecf60781a080033793
Gabriela Suriano <surianogabriela@gmail.com>
2018-11-06 08:55:33 +0000

    automatically backed up by learn

--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -16,8 +16,10 @@ class RestaurantsContainer extends Component {
 }
 
 const mapStateToProps = state => ({ restaurants: state.restaurants })
+
 const mapDispatchToProps = dispatch => ({
   addRestaurant: text => dispatch({ type: "ADD_RESTAURANT", text }),
   deleteRestaurant: id => dispatch({ type: "DELETE_RESTAURANT", id })
 })
+
  export default connect(mapStateToProps, mapDispatchToProps)(RestaurantsContainer)
