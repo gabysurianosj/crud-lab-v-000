1503214cf0dd06d13f184636a14116b7a4e43ffe
Gabriela Suriano <surianogabriela@gmail.com>
2018-11-06 08:49:36 +0000

    automatically backed up by learn

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  state = {
+   text: ''
+ }
+  handleChange(e){
+   this.setState({
+     text: e.target.value
+   })
+ }
+  handleSubmit = (e) => {
+   e.preventDefault();
+   this.props.addRestaurant(this.state.text);
+   this.setState({
+     text: ''
+   })
+ }
+ 
   render() {
     return (
       <div>
